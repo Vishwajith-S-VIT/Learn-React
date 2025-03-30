f52b61850a319096ae78cf96e48c6d2e52165bd7
Vishwajith-S-VIT <vishwajith.s2023@vitstudent.ac.in>
2025-03-30 16:20:45 +0530

    Added Conditions when font size goes below 0.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 import React, { useState } from "react";
 import './AppBackup.css'
 
+function Decrease(fs, setFs){
+    if(fs === 0){
+        console.log("Font size cannot be less than 0px");
+        return;
+    }
+    else{
+        setFs(fs-1);
+    }
+}
+
 export function App(){
     const [fs, setFs] = useState(15);
     return(
@@ -8,7 +18,7 @@ export function App(){
             <h1>Dynamic resizing</h1>
             <h2> Current font size: {fs}px</h2>
             <button onClick={() => setFs(fs+1)}>Increase</button>
-            <button onClick={() => setFs(fs-1)}>Decrease</button>
+            <button onClick={() => Decrease(fs, setFs)}>Decrease</button>
             <p style={{ fontSize: fs }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
     );
