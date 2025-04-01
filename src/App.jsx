a384cbb20a11e47ecac9c6bd057f8dd76d63af58
Vishwajith-S-VIT <vishwajith.s2023@vitstudent.ac.in>
2025-04-01 19:55:39 +0530

    Used HashRouter instead of BrowserRouter to not run into the 404 error.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { App1 } from "./App1";
 import { App2 } from "./App2";
 
@@ -8,8 +8,8 @@ function Home() {
     <div style={{ textAlign: "center" }}>
       <h1>React Router DOM Testing</h1>
       <nav style={{ marginBottom: "20px" }}>
-        <a href="/Learn-React/App1">App1</a>&emsp;|&emsp;
-        <a href="/Learn-Reacts/App2">App2</a>
+        <Link to="/App1">App1</Link>&emsp;|&emsp;
+        <Link to="/App2">App2</Link>
       </nav>
     </div>
   );
@@ -19,9 +19,9 @@ export function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/Learn-React" element={<Home />} />
-        <Route path="/Learn-React/App1" element={<App1 />} />
-        <Route path="/Learn-Reacts/App2" element={<App2 />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/App1" element={<App1 />} />
+        <Route path="/App2" element={<App2 />} />
         <Route path="*" element={<h2>Page Not Found</h2>} />
       </Routes>
     </Router>
